9f31babda7bffbe4bc95b28be91f42db5a181426
Piotr Woloszun <piotr.woloszun@gmail.com>
2015-10-22 15:29:48 +0200

    t 23 fix

--- js/directives/labeledField.js
+++ js/directives/labeledField.js
@@ -1,7 +1,7 @@
 App.directive("labeledField", [
   function() {
     var definition = {
-      templateUrl: 'templates/directive/labeled_field.html',
+      templateUrl: 'templates/directives/labeled_field.html',
       link: function(scope) {
         scope.fieldValue = "some fancy value";
       }
