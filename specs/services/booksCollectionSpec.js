81b9e06feb744fdd381c767b6968225ca50d8c6b
Piotr Woloszun <piotr.woloszun@gmail.com>
2016-04-28 09:29:57 +0200

    booksCollection spec

--- specs/services/booksCollectionSpec.js
+++ specs/services/booksCollectionSpec.js
@@ -1,1 +1,13 @@
 //TODO
+// a) create
+// 1. new length === orignal length + 1
+// 2. last book in collection has expected title
+
+// a) remove
+// 3. new length === orignal length - 1
+// 4. collection does not include removed book
+
+
+// TODO assertions:
+// xxx.should.not.include(yyy)
+// zzz.shouuld.equal(123)
